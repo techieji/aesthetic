b2beff3cd036610062f1bfab27466436353af3e3
Pradhyum Rajasekar <drpradhyum2016@outlook.com>
2024-08-03 13:42:56 -0400

    Weird merge part 2

--- lib.c
+++ lib.c
@@ -36,33 +36,23 @@ void print_value(struct Value* tree) {
         case INT: printf("%d", tree->i); return;
         case NIL: printf("nil"); return;
         case PAIR:
-                  printf("(");
-                  print_value(tree->car);
-                  while ((tree = tree->cdr)->type == PAIR) {
-                      printf(" ");
-                      print_value(tree->car);
-                  }
-                  if (tree->type != NIL) {
-                      printf(" . ");
-                      print_value(tree);
-                  }
-                  printf(")");
-                  return;
-        default: puts("[undefined]");
+            printf("(");
+            print_value(tree->car);
+            while ((tree = tree->cdr)->type == PAIR) {
+                printf(" ");
+                print_value(tree->car);
+            }
+            if (tree->type != NIL) {
+                printf(" . ");
+                print_value(tree);
+            }
+            printf(")");
+            return;
+        case CFN:
+            printf("[compiled function at %p]", tree->cfn);
+            return;
+        default: printf("[undefined]");
     }
-}
-
-struct Value* singleton(enum Type type) {
-    struct Value* token = malloc(sizeof(struct Value));
-    token->type = type;
-    return token;
-}
-
-struct Value* boolean(bool b) {
-    struct Value* token = malloc(sizeof(struct Value));
-    token->type = BOOL;
-    token->b = b;
-    return token;
 }
 
 struct Value* construct(enum Type type, ...) {
@@ -104,6 +94,15 @@ struct Value* construct(enum Type type, ...) {
     return v;
 }
 
+struct Value* construct_list(int len, ...) {
+    va_list l;
+    va_start(l, len);
+    struct Value* v = construct(NIL);
+    for (int i = 0; i < len; i++)
+        v = construct(PAIR, va_arg(l, struct Value*), v);
+    return reverse(v);
+}
+
 char* extract_string(char* s, int i) {
     char* str = malloc((i + 1) * sizeof(char));
     strncpy(str, s, i);
@@ -111,18 +110,10 @@ char* extract_string(char* s, int i) {
     return str;
 }
 
-struct Value* pair(struct Value* v1, struct Value* v2) {
-    struct Value* pair = malloc(sizeof(struct Value));
-    pair->type = PAIR;
-    pair->car = v1;
-    pair->cdr = v2;
-    return pair;
-}
-
 struct Value* reverse(struct Value* l) {
-    struct Value* newl = singleton(NIL);
+    struct Value* newl = construct(NIL);
     while (l->type != NIL) {
-        newl = pair(l->car, newl);
+        newl = construct(PAIR, l->car, newl);
         l = l->cdr;
     }
     return newl;
@@ -160,14 +151,49 @@ bool is_extended_alpha(char c) {
  * STDLIB INITIALIZATION       *
  * * * * * * * * * * * * * * * */
 
-struct Value* add(struct Value* l) {
-    return construct(l->car->i + l->cdr->car->i);     // TODO add accessors
+struct Value* add_two(struct Value* v1, struct Value* v2) {
+    if (v1->type == INT) {
+        if (v2->type == INT) return construct(INT, v1->i + v2->i);
+        if (v2->type == FLOAT) return construct(FLOAT, v1->i + v2->f);
+    }
+    if (v1->type == FLOAT) {
+        if (v2->type == INT) return construct(FLOAT, v1->f + v2->i);
+        if (v2->type == FLOAT) return construct(FLOAT, v1->f + v2->f);
+    }
+    printf("TYPE ERROR IN ADD_TWO. INDUCED SEGFAULT.");
+    return NULL;
+
 }
 
+// TODO add accessors to type check
+struct Value* add(struct Value* args) {
+    struct Value* v = args->car;
+    for (struct Value* l = args->cdr; l->type != NIL; l = l->cdr) {
+        v = add_two(v, l->car);
+    }
+    return v;
+}
+
+struct Value* exit_(struct Value* args) {
+    exit(args->car->i);
+    return NULL;
+}
+
+struct Value* define(struct Value* args, struct Value** env) {
+    struct Value* k = args->car;
+    struct Value* v = eval(args->cdr->car, env);
+    *env = construct(PAIR, construct(PAIR, k, v), *env);
+    return v;
+}
+
+#define DECL(name, type, cfn) construct(PAIR, construct(SYM, name), construct(type, cfn))
+
 struct Value* get_stdlib(void) {
-    struct Value* env = singleton(NIL);
-    env = construct(PAIR,
-            construct(PAIR, construct(SYM, "+"), construct(CFN, add)),
-            construct(NIL));
+    struct Value* env = construct(NIL);
+    env = construct_list(3,
+            DECL("+", CFN, add),
+            DECL("exit", CFN, exit_),
+            DECL("define", CMACRO, define)
+    );
     return env;
 }
--- lib.h
+++ lib.h
@@ -6,11 +6,11 @@ void print_token(struct Value*);
 void print_tree(struct Value*, int);
 void print_value(struct Value*);
 
-//struct Value* singleton(enum Type);
-//struct Value* pair(struct Value*, struct Value*);
-struct Value* reverse(struct Value*);
-
 struct Value* construct(enum Type, ...);
 bool equal_values(struct Value*, struct Value*);
 char* extract_string(char*, int);
 bool is_extended_alpha(char);
+
+struct Value* reverse(struct Value*);
+struct Value* add(struct Value*);
+struct Value* get_stdlib(void);
